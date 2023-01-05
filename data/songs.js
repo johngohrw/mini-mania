2cb55ae7055dfa2a1599439c1a64efa72af0b24c
John Goh <johngohrw@gmail.com>
2023-01-06 02:36:43 +0800

    fix: reorder songs

--- data/songs.js
+++ data/songs.js
@@ -9,38 +9,38 @@ import romance from "../public/songs/1577144 YOASOBI - Romance/YOASOBI - Romance
 
 export const songs = {
   0: {
+    name: "@__@",
+    artist: "Chroma",
+    notes: convertNotes(at__at.notes),
+    url: "./songs/1738412 Chroma - [@__@]/audio.mp3",
+    keys: 7,
+  },
+  1: {
     name: "Yoru ni Kakeru",
     artist: "YOASOBI",
     notes: convertNotes(yoasobiYoruNiKakeruSong.notes, 70),
     url: "./songs/1299810 YOASOBI - Yoru ni Kakeru/audio.mp3",
     keys: 7,
   },
-  1: {
+  2: {
     name: "Sayonara Trip",
     artist: "Dormir",
     notes: convertNotes(testSong.notes),
     url: "./songs/Dormir - Sayonara Trip/Sayonara Trip.mp3",
     keys: 4,
   },
-  2: {
+  3: {
     name: "Koi Yomi Zakura",
     artist: "Loos",
     notes: convertNotes(loosSong.notes, 68.6),
     url: "./songs/128855 Loos - Koi Yomi Zakura/loos - Omoi Yoshino.mp3",
     keys: 7,
   },
-  3: {
+  4: {
     name: "Lapis",
     artist: "SHIKI",
     notes: convertNotes(lapisSong.notes),
     url: "./songs/1652458 SHIKI - Lapis/audio.mp3",
-    keys: 7,
-  },
-  4: {
-    name: "@__@",
-    artist: "Chroma",
-    notes: convertNotes(at__at.notes),
-    url: "./songs/1738412 Chroma - [@__@]/audio.mp3",
     keys: 7,
   },
   5: {
